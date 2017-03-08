68dabb38b3aa109e83c8c1c5ca000b8012022380
jonschlinkert <jon.schlinkert@sellside.com>
2017-03-08 05:09:40 -0500

    add unit tests, lint

--- test.js
+++ test.js
@@ -7,57 +7,84 @@
 
 'use strict';
 
-
+require('mocha');
 var assert = require('assert');
-var should = require('should');
 var re = require('./');
 
-function author(str) {
+function parse(str) {
   return re().exec(str);
 }
 
 describe('author regex:', function () {
-  it('should return a parsed author object.', function () {
+  it('should parse a parsed author object', function () {
     var fixture = 'Jon Schlinkert <jon.schlinkert@sellside.com> (https://github.com/jonschlinkert)';
-    var actual = author(fixture);
+    var actual = parse(fixture);
 
-    actual[1].should.equal('Jon Schlinkert');
-    actual[2].should.equal('jon.schlinkert@sellside.com');
-    actual[3].should.equal('https://github.com/jonschlinkert');
+    assert.equal(actual[1], 'Jon Schlinkert');
+    assert.equal(actual[3], 'jon.schlinkert@sellside.com');
+    assert.equal(actual[5], 'https://github.com/jonschlinkert');
   });
 
-  it('should return name and url.', function () {
+  it('should parse name only', function () {
+    var fixture = 'Jon Schlinkert';
+    var actual = parse(fixture);
+    assert.equal(actual[1], 'Jon Schlinkert');
+  });
+
+  it('should parse name with trailing whitespace', function () {
+    var fixture = 'Jon Schlinkert ';
+    var actual = parse(fixture);
+    assert.equal(actual[1], 'Jon Schlinkert');
+  });
+
+  it('should parse name with leading whitespace', function () {
+    var fixture = ' Jon Schlinkert';
+    var actual = parse(fixture);
+    assert.equal(actual[1], 'Jon Schlinkert');
+  });
+
+  it('should parse name and url', function () {
     var fixture = 'Jon Schlinkert (https://github.com/jonschlinkert)';
-    var actual = author(fixture);
-
-    actual[1].should.equal('Jon Schlinkert');
-    assert.equal(typeof actual[2], 'undefined');
-    actual[3].should.equal('https://github.com/jonschlinkert');
+    var actual = parse(fixture);
+    assert.equal(actual[1], 'Jon Schlinkert');
+    assert.equal(typeof actual[3], 'string');
+    assert.equal(actual[3], 'https://github.com/jonschlinkert');
   });
 
-  it('should return name and url.', function () {
+  it('should parse name and url with no separating space', function () {
     var fixture = 'Jon Schlinkert(https://github.com/jonschlinkert)';
-    var actual = author(fixture);
-
-    actual[1].should.equal('Jon Schlinkert');
-    assert.equal(typeof actual[2], 'undefined');
-    actual[3].should.equal('https://github.com/jonschlinkert');
+    var actual = parse(fixture);
+    assert.equal(actual[1], 'Jon Schlinkert');
+    assert.equal(typeof actual[3], 'string');
+    assert.equal(actual[3], 'https://github.com/jonschlinkert');
   });
 
-  it('should email and url.', function () {
+  it('should parse email and url', function () {
     var fixture = '<jon.schlinkert@sellside.com> (https://github.com/jonschlinkert)';
-    var actual = author(fixture);
-
-    assert.equal(typeof actual[1], 'undefined');
-    actual[2].should.equal('jon.schlinkert@sellside.com');
-    actual[3].should.equal('https://github.com/jonschlinkert');
+    var actual = parse(fixture);
+    assert.equal(typeof actual[1], 'string');
+    assert.equal(actual[3], 'jon.schlinkert@sellside.com');
+    assert.equal(actual[5], 'https://github.com/jonschlinkert');
   });
 
-  it('should return name and email.', function () {
+  it('should parse name and email', function () {
     var fixture = 'Jon Schlinkert <jon.schlinkert@sellside.com>';
-    var actual = author(fixture);
+    var actual = parse(fixture);
 
-    actual[1].should.equal('Jon Schlinkert');
-    actual[2].should.equal('jon.schlinkert@sellside.com');
+    assert.equal(actual[1], 'Jon Schlinkert');
+    assert.equal(actual[3], 'jon.schlinkert@sellside.com');
+  });
+
+  it('should parse email only', function () {
+    var fixture = '<jon.schlinkert@sellside.com>';
+    var actual = parse(fixture);
+
+    assert.equal(actual[3], 'jon.schlinkert@sellside.com');
+  });
+
+  it('should parse url only', function () {
+    var fixture = '(https://github.com/jonschlinkert)';
+    var actual = parse(fixture);
+    assert.equal(actual[3], 'https://github.com/jonschlinkert');
   });
 });
